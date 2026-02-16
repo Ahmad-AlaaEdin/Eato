9c4fe8e72760f8dfd06c6acf42186b7a08aafcea
ahmad <aae22138@gmail.com>
2026-02-16 14:58:17 +0200

    feat: implement initial authentication flow with sign-in, sign-up, shared layout, and reusable UI components for input and buttons, along with Appwrite configuration.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,41 @@
+import { images } from "@/constants";
 import { Slot } from "expo-router";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 export default function _Layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="size-48 absolute -bottom-16  z-10 self-center"
+            resizeMode="contain"
+          />
+        </View>
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.ahmadalaa2.Eato",
   projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseID: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionID: "user",
 };
