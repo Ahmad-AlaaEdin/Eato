13ab532be2c1962656b2d86fab6cd9b221f25a41
ahmad <aae22138@gmail.com>
2026-02-16 14:11:02 +0200

    feat: Integrate Appwrite SDK and configure its environment variables.

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.ahmadalaa2.Eato",
+  projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
